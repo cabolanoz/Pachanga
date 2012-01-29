d4840f840a2897b779210f3dc7570f55b58c067c
Jose Ayerdis <joseayerdis@gmail.com>
2012-01-28 18:34:04 -0600

    Database Contract

---
+++ src/com/pachanga/android/db/DatabaseContract.java
@@ -0,0 +1,61 @@
+package com.pachanga.android.db;
+
+import android.net.Uri;
+
+public class DatabaseContract {
+
+	private final static String LOG = DatabaseContract.class.getName();
+
+	/**
+	 * Columns for a Cierres
+	 * 
+	 * */
+	interface PlacesColumns {
+		String PLACE_ID = "PlaceId";
+		String NAME = "Name";
+		String DESCRIPTION = "Description";
+		String RATE = "Rate";
+		String TYPE = "Type";
+		String COORD_X = "CoordX";
+		String COORD_Y = "CoordY";
+	}
+
+	public static final String CONTENT_AUTHORITY = "com.pachanga.android";
+	public static final Uri BASE_CONTENT_URI = Uri.parse("content://"
+			+ CONTENT_AUTHORITY);
+
+	/**
+	 * Paths for the content uri can be build using this on each definition of
+	 * class
+	 */
+	private static final String PATH_PLACES = "places";
+
+	public static class Places implements PlacesColumns {
+		public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+				.appendPath(PATH_PLACES).build();
+
+		public static String getId(Uri uri) {
+
+			if (uri.getPathSegments().size() >= 2)
+				return uri.getPathSegments().get(1);
+
+			return null;
+		}
+
+		/**
+		 * Specify the content for the record and for this entity
+		 * */
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.pachanga.places";
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.pachanga.places";
+
+		/**
+		 * Default sorting for this entity
+		 * */
+		public static final String DEFAULT_SORT = PlacesColumns.PLACE_ID+ " ASC";
+
+		public static Uri buildUri(String placesId) {
+			return CONTENT_URI.buildUpon().appendPath(placesId).build();
+		}
+
+	}
+}
