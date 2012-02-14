47df2ad6887e5551113b54d4704ce4bc64067022
cbolanos <cesar20904@gmail.com>
2012-02-13 20:41:11 -0800

    Fixing merge trouble

--- src/com/pachanga/android/PlacesListActivity.java
+++ src/com/pachanga/android/PlacesListActivity.java
@@ -3,6 +3,7 @@ package com.pachanga.android;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,25 +22,33 @@ public class PlacesListActivity extends FragmentActivity implements LoaderCallba
 
 	private static String LOG = PlacesListActivity.class.getName();
 	private SimpleCursorAdapter adapter;
+	private PlacesBroadcastReceiver placesBroadcastReceiver = new PlacesBroadcastReceiver();
 	private int refreshCounter = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.places_list);
-//		getApplication().deleteDatabase(DatabaseHelper.database);
+		getApplication().deleteDatabase(DatabaseHelper.database);
 		adapter = new SimpleCursorAdapter(this, R.layout.place_item, null, new String[] {DatabaseContract.Places.NAME, DatabaseContract.Places.ADDRESS}, new int[] {R.id.tvwPlaceName, R.id.tvwPlaceAddress }, 0);
 
 		ListView listView = (ListView) findViewById(R.id.placesList);
 		listView.setAdapter(adapter);
 	}
-
+	
 	@Override
 	public void onResume() {
 		super.onResume();
 		getSupportLoaderManager().restartLoader(0, null, this);
+		registerReceiver(placesBroadcastReceiver, new IntentFilter(ActionsConstant.PLACES_VIEW_ACTION));
 	}
 
+	@Override
+	public void onPause() {
+		super.onPause();
+		unregisterReceiver(placesBroadcastReceiver);
+	}
+	
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle extras) {
 		Uri uri = DatabaseContract.Places.CONTENT_URI;
@@ -79,9 +88,8 @@ public class PlacesListActivity extends FragmentActivity implements LoaderCallba
 	class PlacesBroadcastReceiver extends BroadcastReceiver {
 
 		@Override
-		public void onReceive(Context arg0, Intent arg1) {
-			// TODO Auto-generated method stub
-			
+		public void onReceive(Context context, Intent intent) {
+			Log.i(LOG, "Action: " + intent.getAction());
 		}
 		
 	}
